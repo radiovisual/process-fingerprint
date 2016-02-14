b37c1cb68585eb05aed835109af3cdaf2128ffd5
Michael Wuergler <wuergler@gmail.com>
2016-02-14 21:56:02 +0000

    basic ocd linting

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 'use strict';
 
 module.exports = function () {
-
-    var key = "__processFingerprint";
-
+    var key = '__processFingerprint';
 
     if (process.env.hasOwnProperty(key)) {
-
         return process.env[key];
-
-    } else {
-
-        var d = new Date().getTime();
-        process.env[key] = "_pf" + d;
-        return process.env[key];
-
     }
-
+    var d = new Date().getTime();
+    process.env[key] = '_pf' + d;
+    return process.env[key];
 };
 
